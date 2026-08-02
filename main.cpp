6572715043ff6a0940ba153a9c90027a8eb95f7b
jisookim-kimchi <wltn249@gmail.com>
2026-08-02 19:09:50 +0200

    practiced basic HTTP web serv with while loop and SQ_REUSEADDR feat : Socket Bind Listen Accept Read Send basic flow, config reuse using with SO_REUSEADDR

--- main.cpp
+++ main.cpp
@@ -29,6 +29,18 @@ int main()
     sockAddr.sin_port = htons(port);
 
     /*
+    set + socket + option : config socket options.
+    @param 1 : socket descriptor
+    @param 2 : socket level : SOL_SOCKET - socket options
+    @param 3 : socket option : SO_REUSEADDR - reuse address
+    @param 4 : option value : 1 - enable reuse address
+    @param 5 : option value size
+    @return : 0 on success, -1 on failure
+    */
+    int opt = 1;
+    setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    /*
     @param 1 : socket descriptor
     @param 2 : socket address
     @param 3 : size of socket address
@@ -51,64 +63,64 @@ int main()
     }
     else
         std::cout << "listen : " << ret << std::endl;    
-
-    //4.accept connection
-    //@param 1 : socket fd
-    //@param 2 : pointer to address for client's ip and port number
-    //@param 3 : size for client's ip and port number
-    //@return: fd where request connection is accepted.
-    struct sockaddr_in outClientAddr = {};
-    socklen_t outClientAddrLen = sizeof(outClientAddr);
-    int clientFd = 0;
-    clientFd = accept(serverFd, (struct sockaddr *)&outClientAddr, &outClientAddrLen);
-    if (clientFd < 0)
+    while(1)
     {
-        close(serverFd);
-        return (0);
-    }
-    else
-        std::cout << "accept : " << clientFd << std::endl;
-    
-    char buf[4096] = {0};
-    const int val = read(clientFd, buf, 4096);
-    if(val < 0)
-    {
-        close(clientFd);
-        close(serverFd);
-        return (0);
-    }
-    else
-        std::cout << "read : " << val << std::endl;
-
-    std::string userInput;
-    std::cout << "Enter something : ";
-    std::getline(std::cin, userInput);
-    userInput +="\r\n";
-    // server response message
-    std::string response = 
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type: text/html; charset=UTF-8\r\n"
-    "Content-Length: " + std::to_string(userInput.length()) + "\r\n"
-    "\r\n"
-    + userInput;
-
-    /*
-    @param 1 : socket fd
-    @param 2 : pointer to buffer
-    @param 3 : size of buffer
-    @param 4 : flags : MSG_NOSIGNAL- do not send signal when the client is disconnected
-    @return : 0 on success, -1 on failure
-    */
-    ret = send(clientFd, response.c_str(), response.length(), MSG_NOSIGNAL);
-    if(ret < 0)
-    {
-        close(clientFd);
-        close(serverFd);
-        return (0);
-    }
+        //4.accept connection
+        //@param 1 : socket fd
+        //@param 2 : pointer to address for client's ip and port number
+        //@param 3 : size for client's ip and port number
+        //@return: fd where request connection is accepted.
+        struct sockaddr_in outClientAddr = {};
+        socklen_t outClientAddrLen = sizeof(outClientAddr);
+        int clientFd = 0;
+        clientFd = accept(serverFd, (struct sockaddr *)&outClientAddr, &outClientAddrLen);
+        if (clientFd < 0)
+        {
+            close(serverFd);
+            return (0);
+        }
+        else
+            std::cout << "accept : " << clientFd << std::endl;
         
-    if (close(clientFd) < 0)
-        return (0);
+        char buf[4096] = {0};
+        const int val = read(clientFd, buf, 4096);
+        if(val < 0)
+        {
+            close(clientFd);
+            continue;
+        }
+        else
+            std::cout << "read : " << val << std::endl;
+
+        std::string userInput;
+        std::cout << "Enter something : ";
+        std::getline(std::cin, userInput);
+        userInput +="\r\n";
+        // server response message
+        std::string response = 
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html; charset=UTF-8\r\n"
+        "Content-Length: " + std::to_string(userInput.length()) + "\r\n"
+        "\r\n"
+        + userInput;
+
+        /*
+        @param 1 : socket fd
+        @param 2 : pointer to buffer
+        @param 3 : size of buffer
+        @param 4 : flags : MSG_NOSIGNAL- do not send signal when the client is disconnected
+        @return : 0 on success, -1 on failure
+        */
+        ret = send(clientFd, response.c_str(), response.length(), MSG_NOSIGNAL);
+        if(ret < 0)
+        {
+            close(clientFd);
+            close(serverFd);
+            return (0);
+        }
+        if (close(clientFd) < 0)
+            return (0);
+    }
     if (close(serverFd) < 0)
         return (0);
     return 1;
